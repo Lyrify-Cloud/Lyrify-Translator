8eebe30a81c9bc19f39f5f3d94268d11659c9e61
sipc.ink <92251518+SIPC@users.noreply.github.com>
2024-02-09 23:46:19 +0800

    Update autodetect.ts

--- src/pages/api/lib/autodetect.ts
+++ src/pages/api/lib/autodetect.ts
@@ -10,17 +10,20 @@ const detectionMap: Record<string, string> = {
 export async function autodetect(content: string): Promise<string> {
   try {
     let data = JSON.stringify({"text": content})
-    const langResponse = await axios.post(`https://language.api.sipc.ink/language-identifier/identify`,data);
+    const langResponse = await axios.post(`https://api.translatedlabs.com/language-identifier/identify`,data);
     const detectedLang = langResponse.data.code.substring(0, 2).toLowerCase();
     return detectedLang;
   } catch (axiosError) {
     console.error("Axios Error:", axiosError);
     try {
-      const detectedLang = detectOne(content);
-      return detectionMap[detectedLang] || detectedLang || "auto";
+      return localDetect(content);
     } catch (langdetectError) {
       console.error("Langdetect Error:", langdetectError);
       return "auto";
     }
   }
+}
+function localDetect(content: string): string {
+  const detectedLang = detectOne(content);
+  return detectionMap[detectedLang] || detectedLang || "auto";
 }
